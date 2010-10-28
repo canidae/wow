964a71e9e401a0e5852e9e70d9678cb16a950de3
Vidar Wahlberg <canidae@exent.net>
2010-10-28 23:30:16 +0200

    still working on loyalty, goes slow, but meh

--- Loyalty/Loyalty.lua
+++ Loyalty/Loyalty.lua
@@ -1,45 +1,110 @@
 Loyalty = CreateFrame("Frame")
-Loyalty.mendPet = "Mend Pet"
+Loyalty.aspects = {
+	"Aspect of the Hawk",
+	"Aspect of the Fox",
+	"Aspect of the Pack",
+	"Aspect of the Cheetah",
+	"Aspect of the Wild"
+}
+Loyalty.abilities = {
+	["Black Arrow"] = "player",
+	["Explosive Shot"] = "player",
+	["Serpent Sting"] = "player",
+	["Scatter Shot"] = "player",
+	["Mend Pet"] = "pet",
+	["Wyvern Sting"] = "player",
+	["Disengage"] = "player",
+	["Deterrence"] = "player",
+	["Flare"] = "player",
+	["Bestial Wrath"] = "player",
+	["Intimidation"] = "player",
+	["Master's Call"] = "player",
+	["Medallion of the Horde"] = "player",
+	["Kill Command"] = "player",
+	["Concussive Shot"] = "player",
+	["Arcane Torrent"] = "player",
+	["Feign Death"] = "player"
+}
+Loyalty.mend = "Mend Pet"
 
-function Loyalty:OnUpdate()
+function Loyalty:OnUpdate(elapsed)
+	local playerhealth = math.ceil(UnitHealth("player") * 100 / UnitHealthMax("player"))
+	local r, g, b
+	if playerhealth > 80 then
+		r = 0.0
+		g = 1.0
+		b = 0.0
+	elseif playerhealth > 20 then
+		r = 1.0
+		g = 1.0
+		b = 0.0
+	else
+		r = 1.0
+		g = 0.0
+		b = 0.0
+	end
+	LoyaltyPlayerHealthTexture:SetTexture(r, g, b)
+	LoyaltyPlayerHealthText:SetText(playerhealth)
+	local playerpowermax = UnitPowerMax("player")
+	local playerpower = UnitPower("player")
+	if playerpower < 20 then
+		-- red, most likely no damage abilities can be used
+		r = 1.0
+		g = 0.0
+		b = 0.0
+	elseif playerpower < 45 or playerpower > 90 then
+		-- yellow, some damage abilites can be used, or we're about to cap focus
+		r = 1.0
+		g = 1.0
+		b = 0.0
+	else
+		-- green, all abilities can be used
+		r = 0.0
+		g = 1.0
+		b = 0.0
+	end
+	LoyaltyPlayerPowerBar:SetMinMaxValues(0, playerpowermax)
+	LoyaltyPlayerPowerBar:SetValue(playerpower)
+	LoyaltyPlayerPowerBarText:SetText(playerpower)
+	LoyaltyPlayerPowerBarTexture:SetVertexColor(r, g, b)
+
+	local status
+
 	if UnitExists("pet") then
-		local petHealth = UnitHealth("pet")
-		local petHealthMax = UnitHealthMax("pet")
-		local _, _, _, _, _, duration, expires, _, _, _, _ = UnitBuff("pet", Loyalty.mendPet)
-		local now = GetTime()
-		local odd = (now * 2 - math.floor(now * 2) < 0.5)
-		local inRange = (IsSpellInRange(Loyalty.mendPet, "pet") == 1)
+		local pethealth = math.ceil(UnitHealth("pet") * 100 / UnitHealthMax("pet"))
+		local r, g, b
+		if pethealth > 80 then
+			r = 0.0
+			g = 1.0
+			b = 0.0
+		elseif pethealth > 20 then
+			r = 1.0
+			g = 1.0
+			b = 0.0
+		else
+			r = 1.0
+			g = 0.0
+			b = 0.0
+		end
+		LoyaltyPetHealthTexture:SetTexture(r, g, b, 1)
+		LoyaltyPetHealthText:SetText(pethealth)
+
+		if IsSpellInRange(Loyalty.mend, "pet") ~= 1 then
+			status = UnitName("pet") .. " is too far away!"
+		end
+
+		local _, _, _, _, _, duration, expires, _, _, _, _ = UnitBuff("pet", Loyalty.mend)
+		if UnitIsDead("pet") then
+			status = "You let " .. UnitName("pet") .. " die! Bastard!"
+		elseif not duration and pethealth < 95 and InCombatLockdown() and (UnitIsPVP("player") or pethealth < 75) then
+			-- pet is wounded and mend pet is not up, annoy hunter
+			if not status then
+				status = "Heal " .. UnitName("pet") .. "!"
+			end
+			PlaySound("igQuestFailed")
+		end
+		--[[
 		local value = 0
-		LoyaltyFrame:Show();
-		LoyaltyHealthBar:SetMinMaxValues(0, petHealthMax)
-		LoyaltyHealthBarText:SetText(UnitName("pet"))
-		if not duration and petHealth / petHealthMax < 0.95 then
-			-- pet is wounded and mend pet is not up, flash pet health bar
-			if odd then
-				if not inRange then
-					-- pet is not in range, flash red
-					LoyaltyHealthBarTexture:SetVertexColor(1.0, 0.0, 0.0, 1.0)
-				else
-					-- pet is in range, flash yellow
-					LoyaltyHealthBarTexture:SetVertexColor(1.0, 1.0, 0.0, 1.0)
-					if InCombatLockdown() and (UnitIsPVP("player") or petHealth / petHealthMax < 0.75) then
-						-- play sound alerting us to heal
-						PlaySound("igQuestFailed")
-					end
-				end
-				LoyaltyHealthBar:SetValue(petHealthMax)
-			end
-		elseif not inRange then
-			-- not wounded, but neither in range, flash white
-			if odd then
-				LoyaltyHealthBarTexture:SetVertexColor(1.0, 1.0, 1.0, 1.0)
-				LoyaltyHealthBar:SetValue(petHealthMax)
-			end
-		end
-		if not odd then
-			LoyaltyHealthBarTexture:SetVertexColor(0.0, 1.0, 0.0, 1.0)
-			LoyaltyHealthBar:SetValue(petHealth)
-		end
 		if duration and expires then
 			-- mend pet is up, show remaining duration on mend pet bar
 			LoyaltyMendPetBar:SetMinMaxValues(0, duration)
@@ -47,26 +112,7 @@ function Loyalty:OnUpdate()
 			value = expires - now
 		end
 		LoyaltyMendPetBar:SetValue(value)
-		local start, duration, _ = GetSpellCooldown("Bestial Wrath")
-		if start and start > 0 then
-			LoyaltyBestialWrathBar:SetMinMaxValues(0, duration)
-			LoyaltyBestialWrathBarText:SetTextColor(1.0, 0.0, 0.0, 1.0)
-			value = (start + duration) - now
-		else
-			LoyaltyBestialWrathBarText:SetTextColor(0.0, 1.0, 0.0, 1.0)
-			value = 0
-		end
-		LoyaltyBestialWrathBar:SetValue(value)
-		start, duration, _ = GetSpellCooldown("Intimidation")
-		if start and start > 0 then
-			LoyaltyIntimidationBar:SetMinMaxValues(0, duration)
-			LoyaltyIntimidationBarText:SetTextColor(1.0, 0.0, 0.0, 1.0)
-			value = (start + duration) - now
-		else
-			LoyaltyIntimidationBarText:SetTextColor(0.0, 1.0, 0.0, 1.0)
-			value = 0
-		end
-		LoyaltyIntimidationBar:SetValue(value)
+
 		start, duration, _ = GetSpellCooldown("Master's Call")
 		if start and start > 0 then
 			LoyaltyMastersCallBar:SetMinMaxValues(0, duration)
@@ -76,10 +122,29 @@ function Loyalty:OnUpdate()
 			LoyaltyMastersCallBarText:SetTextColor(0.0, 1.0, 0.0, 1.0)
 			value = 0
 		end
-		LoyaltyMastersCallBar:SetValue(value)
+		--]]
 	else
-		LoyaltyFrame:Hide();
+		LoyaltyPetHealthTexture:SetTexture(0, 0, 0, 0)
+		LoyaltyPetHealthText:SetText("")
+		if not status and not IsMounted() then
+			status = "Where's your companion?"
+		end
 	end
+
+	if not status then
+		local aspectactive
+		for index, aspect in pairs(Loyalty.aspects) do
+			if UnitBuff("player", aspect) then
+				aspectactive = 1
+				break
+			end
+		end
+		if not aspectactive then
+			status = "No aspect?"
+		end
+	end
+
+	LoyaltyFrameStatus:SetText(status or "")
 end
 
 Loyalty:SetScript("OnUpdate", Loyalty.OnUpdate)
