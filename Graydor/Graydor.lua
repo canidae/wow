3507c9774b06f2a7d1fcb4981c339cb04ba69271
Vidar Wahlberg <canidae@exent.net>
2010-11-01 19:30:45 +0100

    xml->lua

---
+++ Graydor/Graydor.lua
@@ -0,0 +1,19 @@
+Graydor = CreateFrame("Frame", nil, "MerchantFrame")
+
+function Graydor:Sell(...)
+	ClearCursor()
+	for bag = 0, NUM_BAG_SLOTS do
+		for slot = 1, GetContainerNumSlots(bag) do
+			local texture, _, locked = GetContainerItemInfo(bag, slot)
+			if texture and not locked then
+				local itemlink = GetContainerItemLink(bag, slot)
+				local itemname, _, quality, _, _, _, _, _, _, _, itemvendorprice = GetItemInfo(itemlink)
+				if quality == 0 and itemvendorprice and itemvendorprice > 0 then
+					UseContainerItem(bag, slot)
+				end
+			end
+		end
+	end
+end
+
+Graydor:SetScript("OnShow", Graydor.Sell)
