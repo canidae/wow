2177368d94c9c0e41e98ef031a8a1789dee73109
Vidar Wahlberg <canidae@exent.net>
2010-11-06 00:24:30 +0100

    Profit working-ish again now

--- Profit/Profit.lua
+++ Profit/Profit.lua
@@ -13,15 +13,11 @@ function Profit:OnEvent(event, arg1, ...)
 			profit_db = {}
 		end
 		if not profit_db.items then
-			-- item data retrieved from scanning AH
+			-- item data
 			profit_db.items = {}
 		end
-		if not profit_db.sellitems then
-			-- items to sell and their preferred stack size
-			profit_db.sellitems = {}
-		end
 		if not profit_db.selltime then
-			-- how long to put up the item (1 = 8h, 2 = 24h, 3 = 48h)
+			-- how long to put up the item (1 = 8h, 2 = 24h, 3 = 48h, any other value is duration in minutes)
 			profit_db.selltime = 2
 		end
 		if not profit_db.sellbidpercent then
@@ -36,12 +32,56 @@ function Profit:OnEvent(event, arg1, ...)
 			-- how much profit we at least want to make, relative to vendor price
 			profit_db.sellprofitpercent = 1.80
 		end
+		-- set sell time in auction frame to our global sell time
+		AuctionFrameAuctions.duration = profit_db.selltime
+		-- some hooking
+		AuctionsStackSizeEntry:HookScript("OnEditFocusLost", Profit.RememberStackSize)
+		AuctionsStackSizeMaxButton:HookScript("OnClick", Profit.RememberStackSize)
+		hooksecurefunc("DurationDropDown_OnClick", Profit.RememberDuration)
 	elseif event == "AUCTION_ITEM_LIST_UPDATE" then
 		Profit:SetScript("OnUpdate", Profit.OnUpdate)
 	elseif event == "NEW_AUCTION_UPDATE" then
-		print(event)
 		Profit:AuctionItem()
 	end
+end
+
+function Profit:RememberStackSize()
+	local itemname, _, _, _, _, _, _, maxstacksize, totalcount = GetAuctionSellItemInfo()
+	if not itemname then
+		return
+	end
+	if not profit_db.items[itemname] then
+		profit_db.items[itemname] = {}
+	end
+	local stacksize = AuctionsStackSizeEntry:GetNumber()
+	if stacksize == maxstacksize or stacksize == totalcount then
+		profit_db.items[itemname].stacksize = nil
+		print(Profit.msg, "Using max stack size when selling", itemname)
+	else
+		profit_db.items[itemname].stacksize = stacksize
+		print(Profit.msg, "Using stack size of", stacksize, "when selling", itemname)
+	end
+	AuctionsNumStacksMaxButton:Click()
+end
+
+function Profit:RememberDuration()
+	local itemname = GetAuctionSellItemInfo()
+	local msg
+	if not itemname then
+		profit_db.selltime = AuctionFrameAuctions.duration
+		msg = "Global sell time"
+	else
+		if not profit_db.items[itemname] then
+			profit_db.items[itemname] = {}
+		end
+		profit_db.items[itemname].selltime = AuctionFrameAuctions.duration
+		msg = "Sell time for " .. itemname
+	end
+	local textduration = UIDropDownMenu_GetText(DurationDropDown)
+	if textduration == CUSTOM then
+		textduration = AuctionFrameAuctions.duration
+	end
+	print(Profit.msg, msg, "set to", textduration)
 end
 
 function Profit:OnUpdate()
@@ -83,7 +123,7 @@ function Profit:OnUpdate()
 	end
 	if wasnil > 0 then
 		-- TODO: can we improve this? rescanning page won't work (we'll get duplicate data for items we got link to)
-		print("missing itemlink for", wasnil, "items on page", Profit.page, ", skipping items")
+		print(Profit.msg, "Missing itemlink for", wasnil, "items on page", Profit.page, ", skipping items")
 	end
 	-- update status bar
 	Profit.page = Profit.page + 1
@@ -156,31 +196,21 @@ function Profit:SetItemData(itemname, minprice, average, stddev, myprice, auctio
 	-- add item to database
 	local hour, minute = GetGameTime()
 	local _, month, day, year = CalendarGetDate()
-	local item = profit_db.items[itemname]
-	if item then
-		-- keep minprice/average/stddev if we got no new data for these
-		if not minprice then
-			minprice = item.minprice
-		end
-		if not average then
-			average = item.average
-		end
-		if not stddev then
-			stddev = item.stddev
-		end
+	if not profit_db.items[itemname] then
+		profit_db.items[itemname] = {}
 	end
-	profit_db.items[itemname] = {
-		minprice = minprice,
-		average = average,
-		stddev = stddev,
-		myprice = myprice,
-		auctions = auctions,
-		year = year,
-		month = month,
-		day = day,
-		hour = hour,
-		minute = minute
-	}
+	local item = profit_db.items[itemname]
+	-- keep minprice/average/stddev/myprice if we got no new data for these
+	item.minprice = minprice or item.minprice
+	item.average = average or item.average
+	item.stddev = stddev or item.stddev
+	item.myprice = myprice or item.myprice
+	item.auctions = auctions
+	item.year = year
+	item.month = month
+	item.day = day
+	item.hour = hour
+	item.minute = minute
 end
 
 function Profit:StartScan(itemname)
@@ -190,12 +220,16 @@ function Profit:StartScan(itemname)
 	ProfitScanButton:Disable()
 	Profit.scan = itemname
 	Profit.page = 0
-	Profit.items = {}
+	if Profit.items then
+		wipe(Profit.items)
+	else
+		Profit.items = {}
+	end
 	QueryAuctionItems(Profit.scan, 0, 0, 0, 0, 0, Profit.page, 0, 0, 0)
 end
 
 function Profit:AuctionItem()
-	local itemname, _, count, _, _, _, itemvendorprice, maxstackcount, totalcount = GetAuctionSellItemInfo();
+	local itemname, _, count, _, _, _, itemvendorprice, maxstacksize, totalcount = GetAuctionSellItemInfo()
 	if not itemname then
 		return
 	end
@@ -213,35 +247,40 @@ function Profit:AuctionItem()
 			-- item not in db, add it
 			Profit:SetItemData(itemname)
 		end
+		AuctionsCreateAuctionButton:SetText("Scanning...")
 		Profit:StartScan(itemname)
 		return
 	end
+	AuctionsCreateAuctionButton:SetText(CREATE_AUCTION)
 	local _, itemlink = GetItemInfo(itemname)
 	local buyoutprice = Profit:GetBuyoutPricePerItem(itemname)
 	if not buyoutprice then
-		print("Unable to find price for " .. itemlink .. ", possibly never seen the item on the Auction House")
+		print(Profit.msg, "Unable to find price for " .. itemlink .. ", possibly never seen the item on the Auction House")
 		return
 	end
 	-- we got what we need of information to sell this item
-	local stackcount = 10
-	local stacks = math.max(math.floor(totalcount / stackcount), 1)
+	local stacksize = item.stacksize or math.min(totalcount, maxstacksize)
+	local stacks = math.max(math.floor(totalcount / stacksize), 1)
 	buyoutprice = math.floor(buyoutprice)
 	local bidprice = math.floor(buyoutprice * profit_db.sellbidpercent)
-	if bidprice > itemvendorprice * profit_db.sellprofitpercent then
-		-- will earn enough
-		AuctionsStackSizeEntry:SetNumber(stackcount)
-		AuctionsNumStacksEntry:SetNumber(stacks)
-		AuctionFrameAuctions.priceType = 1 -- should really be PRICE_TYPE_UNIT, but that is apparently local :\
-		UIDropDownMenu_SetText(PriceDropDown, AUCTION_PRICE_PER_ITEM)
-		MoneyInputFrame_SetCopper(StartPrice, bidprice)
-		MoneyInputFrame_SetCopper(BuyoutPrice, buyoutprice)
-		AuctionFrameAuctions.duration = profit_db.selltime
-		UIDropDownMenu_SetText(DurationDropDown, AUCTION_DURATION_TWO)
-		UpdateDeposit()
-		AuctionsFrameAuctions_ValidateAuction()
-	else
-		-- won't earn enough, print warning
-		print("Low profit for", itemlink .. ": |cffff0000", math.floor(bidprice * 100 / itemvendorprice - 100) .. "%/" .. math.floor(buyoutprice * 100 / itemvendorprice - 100) .. "%|r")
+	local selltime = item.selltime or profit_db.selltime
+	local deposit = CalculateAuctionDeposit(selltime, 1, 1)
+	-- update fields
+	AuctionsStackSizeEntry:SetNumber(stacksize)
+	AuctionsNumStacksEntry:SetNumber(stacks)
+	AuctionFrameAuctions.priceType = 1 -- should really be PRICE_TYPE_UNIT, but that is apparently local :\
+	PriceDropDown:Hide() -- to update the dropdown value
+	PriceDropDown:Show() -- ditto ^^
+	MoneyInputFrame_SetCopper(StartPrice, bidprice)
+	MoneyInputFrame_SetCopper(BuyoutPrice, buyoutprice)
+	AuctionFrameAuctions.duration = selltime
+	DurationDropDown:Hide() -- to update the dropdown value
+	DurationDropDown:Show() -- ditto ^^
+	UpdateDeposit()
+	AuctionsFrameAuctions_ValidateAuction()
+	-- print warning if profit is low
+	if bidprice < itemvendorprice * profit_db.sellprofitpercent + deposit then
+		print(Profit.msg, "Low profit for", itemlink .. ":|cffff0000", math.floor(bidprice * 100 / (itemvendorprice + deposit) - 100) .. "%/" .. math.floor(buyoutprice * 100 / (itemvendorprice + deposit) - 100) .. "%|r")
 	end
 end
 
@@ -323,22 +362,13 @@ function Profit:UpdateTooltip()
 			GameTooltip:AddLine("Rivaling auctions: " .. item.auctions)
 		end
 		SetTooltipMoney(GameTooltip, math.floor(buyoutprice * count), nil, count .. "x@AH:", nil)
-		local stacksize = profit_db.sellitems[itemname]
-		if stacksize then
-			local _, _, _, _, _, _, _, _, _, _, itemvendorprice = GetItemInfo(itemlink)
-			if not itemvendorprice or itemvendorprice < 1 then
-				itemvendorprice = 1
-			end
-			local profit = buyoutprice / itemvendorprice
-			local profittext = math.floor(profit * 1000) / 10
-			GameTooltip:AddLine("Auction (" .. profittext .. "%, stack: " .. stacksize .. ")")
-		end
 	end
 end
 
 Profit.eventtimes = {}
 Profit.page = 0
 Profit.daysinmonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+Profit.msg = "|cff81c30f[Profit]|r"
 
 Profit:SetScript("OnEvent", Profit.OnEvent)
 
