5f0277c07de4d86900df0fc5092a792a2a1eb3b9
Vidar Wahlberg <canidae@exent.net>
2011-03-06 01:11:49 +0100

    sorting in commodity doesn't work

--- Commodity/Commodity.lua
+++ Commodity/Commodity.lua
@@ -7,11 +7,15 @@ function SlashCmdList.Commodity(msg)
 	if command == "reserve" and rest then
 		local stackSize, item = rest:match("^%s*(%d*)%s*(.+)%s*$")
 		if item then
+			local itemName = GetItemInfo(item)
+			if itemName then
+				item = itemName
+			end
 			if stackSize and stackSize ~= "" then
 				stackSize = tonumber(stackSize)
 				print("Reserving slots for", item, "in stacks of", stackSize)
 			else
-				stackSize = nil
+				stackSize = 0
 				print("Reserving slots for", item)
 			end
 			for slot = 1, MAX_GUILDBANK_SLOTS_PER_TAB do
@@ -21,7 +25,7 @@ function SlashCmdList.Commodity(msg)
 				end
 				local column = math.ceil((slot - 0.5) / NUM_SLOTS_PER_GUILDBANK_GROUP)
 				local button = _G["GuildBankColumn" .. column .. "Button" .. index]
-				if not Commodity.reserving then
+				if not Commodity.reserveItem then
 					-- store original functions when we're reserving slots
 					Commodity["OnClick" .. slot] = button:GetScript("OnClick")
 					Commodity["OnDragStart" .. slot] = button:GetScript("OnDragStart")
@@ -30,23 +34,35 @@ function SlashCmdList.Commodity(msg)
 					Commodity["OnReceiveDrag" .. slot] = button:GetScript("OnReceiveDrag")
 					-- set up new functions
 					button:SetScript("OnMouseDown", function(...)
-						Commodity:Reserve(slot, item, stackSize)
+						if not IsShiftKeyDown() then
+							-- in case we're linking item
+							Commodity:Reserve(slot)
+						end
 					end)
-					button:SetScript("OnEnter", function(self, motion)
-						Commodity:Reserve(slot, item, stackSize)
-					end)                                                                                                                                                                              
-					button:SetScript("OnClick", nil)                                                                                                                                                  
+					button:SetScript("OnEnter", function(...)
+						Commodity:Reserve(slot)
+						-- also call old OnEnter to show tooltip
+						Commodity["OnEnter" .. slot](...)
+					end)
+					button:SetScript("OnClick", function(...)
+						-- we may still want to link, so if shift is down, call original function
+						if IsShiftKeyDown() then
+							Commodity["OnClick" .. slot](...)
+						end
+					end)
 					button:SetScript("OnDragStart", nil)
 					button:SetScript("OnReceiveDrag", nil)
 				end
 			end
-			Commodity.reserving = 1
+			Commodity.reserveItem = item
+			Commodity.reserveStackSize = stackSize
 		end
-	elseif command == "done" and Commodity.reserving then
+	elseif command == "done" and Commodity.reserveItem then
+		print("Done reserving slots")
 		for slot = 1, MAX_GUILDBANK_SLOTS_PER_TAB do
 			local index = math.fmod(slot, NUM_SLOTS_PER_GUILDBANK_GROUP)
 			if index == 0 then
-				index = NUM_SLOTS_PER_GUILDBANK_GROUP                                                                                                                            
+				index = NUM_SLOTS_PER_GUILDBANK_GROUP
 			end
 			local column = math.ceil((slot - 0.5) / NUM_SLOTS_PER_GUILDBANK_GROUP)
 			local button = _G["GuildBankColumn" .. column .. "Button" .. index]
@@ -56,7 +72,8 @@ function SlashCmdList.Commodity(msg)
 			button:SetScript("OnMouseDown", Commodity["OnMouseDown" .. slot])
 			button:SetScript("OnReceiveDrag", Commodity["OnReceiveDrag" .. slot])
 		end
-		Commodity.reserving = nil
+		Commodity.reserveItem = nil
+		Commodity.reserveStackSize = nil
 
 		-- update guild bank tab text
 		for tab, tabdata in pairs(Commodity.tabs) do
@@ -66,7 +83,7 @@ function SlashCmdList.Commodity(msg)
 			if pos and pos > 1 then
 				text = tabtext:sub(1, pos - 1)
 			elseif not pos then
-				text = text .. tabtext
+				text = text .. tabtext .. "\n"
 			end
 			text = text .. "[Commodity]\n"
 			for item, itemdata in pairs(tabdata.items) do
@@ -83,28 +100,31 @@ function SlashCmdList.Commodity(msg)
 				end
 				text = text .. "\n"
 			end
-			text = text .. "\n[/Commodity]"
+			text = text .. "[/Commodity]"
 			_, pos = tabtext:find("%[/Commodity%]")
 			if pos then
 				text = text .. tabtext:sub(pos + 1)
 			end
 			SetGuildBankText(tab, text)
 		end
+	else
+		print("Commodity usage:")
+		print("/commodity reserve [stack size] <item name>")
+		print("/commodity done")
 	end
 end
 
-function Commodity:Reserve(slot, item, stackSize)
+function Commodity:Reserve(slot)
 	local tab = GetCurrentGuildBankTab()
 	if not tab or not slot then
 		return
 	end
 	if IsMouseButtonDown("LeftButton") then
-		Commodity.tabs[tab].items[item].stackSize = stackSize
-		Commodity.tabs[tab].items[item].slots[slot] = 1
-		Commodity.tabs[tab].slots[slot].items[item] = 1
+		Commodity:SetItemData(tab, slot, Commodity.reserveItem, Commodity.reserveStackSize)
+		Commodity:SetGuildBankSlotOverlay(tab, slot)
 	elseif IsMouseButtonDown("RightButton") then
-		Commodity.tabs[tab].items[item].slots[slot] = nil
-		Commodity.tabs[tab].slots[slot].items[item] = nil
+		Commodity:SetItemData(tab, slot, Commodity.reserveItem, nil)
+		Commodity:SetGuildBankSlotOverlay(tab, slot)
 	end
 end
 
@@ -131,14 +151,10 @@ function Commodity:OnEvent(event, arg1, ...)
 		local tab = tonumber(arg1)
 		local text = GetGuildBankText(tab) or ""
 		local _, pos = text:find("%[Commodity%]")
+		if Commodity.tabs[tab] then
+			wipe(Commodity.tabs[tab])
+		end
 		if pos then
-			if Commodity.tabs[tab] then
-				wipe(Commodity.tabs[tab])
-			end
-			Commodity.tabs[tab] = {
-				items = {},
-				slots = {}
-			}
 			for line in text:sub(pos + 1):gmatch("[^\n]+") do
 				line = strtrim(line)
 				if line ~= "" then
@@ -149,22 +165,13 @@ function Commodity:OnEvent(event, arg1, ...)
 					if not slots then
 						print("Unable to parse line: " .. line)
 					else
-						if not Commodity.tabs[tab].items[item] then
-							Commodity.tabs[tab].items[item] = {
-								slots = {},
-								stackSize = tonumber(stackSize)
-							}
-						end
 						local length = strlen(slots)
 						for start = 1, length, 2 do
 							local slot = tonumber(strsub(slots, start, start + 1))
-							if not Commodity.tabs[tab].slots[slot] then
-								Commodity.tabs[tab].slots[slot] = {
-									items = {}
-								}
+							if not stackSize or strtrim(stackSize) == "" then
+								stackSize = 0
 							end
-							Commodity.tabs[tab].items[item].slots[slot] = 1
-							Commodity.tabs[tab].slots[slot].items[item] = 1
+							Commodity:SetItemData(tab, slot, item, stackSize)
 						end
 
 					end
@@ -220,7 +227,6 @@ function Commodity:SortGuildBankTab()
 						ClearCursor()
 						SplitGuildBankItem(tab, slot, moveAmount)
 						PickupGuildBankItem(tab, slot2)
-						print(moveAmount, itemName, "from", slot, "to", slot2, "replacing", itemName2, ":", priority, priority2, "|", itemLevel, itemLevel2, "|", stackSize, stackSize2, "|", amount, amount2)
 						return
 					end
 				end
@@ -287,7 +293,7 @@ function Commodity:SetItemData(tab, slot, item, stackSize)
 	if not tab or not slot or not item then
 		return
 	end
-	if not Commodity.tabs[tab] then
+	if not Commodity.tabs[tab] or not Commodity.tabs[tab].slots or not Commodity.tabs[tab].items then
 		Commodity.tabs[tab] = {
 			slots = {},
 			items = {}
@@ -304,7 +310,6 @@ function Commodity:SetItemData(tab, slot, item, stackSize)
 			slots = {}
 		}
 	end
-	stackSize = tonumber(stackSize)
 	tabdata.slots[slot].items[item] = stackSize
 	tabdata.items[item].slots[slot] = stackSize
 end
@@ -321,26 +326,32 @@ end
 
 function Commodity:SetGuildBankSlotOverlay(tab, slot)
 	local overlaytext = _G["CommodityOverlayText" .. slot]
-	if Commodity.tabs[tab] and Commodity.tabs[tab].slots[slot] then
-		local reservations = 0
-		for item, stackSize in pairs(Commodity.tabs[tab].slots[slot]) do
+	local reservations = 0
+	if Commodity.tabs[tab] and Commodity.tabs[tab].slots[slot] and Commodity.tabs[tab].slots[slot].items then
+		for item, stackSize in pairs(Commodity.tabs[tab].slots[slot].items) do
 			reservations = reservations + 1
 		end
-		overlaytext:SetText(reservations)
-	else
+	end
+	if reservations == 0 then
 		overlaytext:SetText("")
+	else
+		overlaytext:SetText(reservations)
 	end
 end
 
 function Commodity:UpdateTooltip(tab, slot)
 	if Commodity.tabs[tab] and Commodity.tabs[tab].slots[slot] then
 		local headerAdded
-		for item, _ in pairs(Commodity.tabs[tab].slots[slot].items) do
+		for item, stackSize in pairs(Commodity.tabs[tab].slots[slot].items) do
 			if not headerAdded then
 				headerAdded = 1
 				GameTooltip:AddLine("Commodity reservations:")
 			end
-			GameTooltip:AddDoubleLine(item, Commodity.tabs[tab].items[item].stackSize, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0)
+			if stackSize == 0 then
+				-- 0 denotes no limit on stack size, don't show stack size for item
+				stackSize = nil
+			end
+			GameTooltip:AddDoubleLine(item, stackSize, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0)
 		end
 		if headerAdded then
 			GameTooltip:Show()
